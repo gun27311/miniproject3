2489d51fabeb5740d375450139349cf77e7ce6e3
gun27311 <58160698@go.buu.ac.th>
2017-05-02 17:11:23 +0700

    fig controller homepage

---
+++ application/views/Homepage.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>คำสั่งแต่งตั้งของข้า</h1>
+    </body>
+
+</html>
